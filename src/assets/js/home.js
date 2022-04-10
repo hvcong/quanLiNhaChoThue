052e8f914436916636cf1a9d256ec2fffbb4c705
hvcong <hvcong101201@gmail.com>
2022-04-10 15:50:24 +0700

    update all

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,11 +1,11 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-let viewWidth = window.innerWidth
-let viewHeight = window.innerHeight
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+// let viewWidth = window.innerWidth
+// let viewHeight = window.innerHeight
 
-//when document resize
-window.addEventListener('resize', e => {
-    viewWidth = e.target.innerWidth
-    viewHeight = e.target.innerHeight
-    handleSlide()
-})
+// //when document resize
+// window.addEventListener('resize', e => {
+//     viewWidth = e.target.innerWidth
+//     viewHeight = e.target.innerHeight
+//     handleSlide()
+// })
