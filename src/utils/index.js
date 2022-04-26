eb47adace4ec94e304efde791fa82b465f2ba998
hvcong <hvcong101201@gmail.com>
2022-04-26 16:27:26 +0700

    reponsive for header, update home

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,20 @@
-// viết các func có thể chia sẻ giữa các page
+
+
+// handle header on mobile device
+
+let menuBarElement = document.getElementById("menu__bar");
+let menuLayerElement = document.getElementById("menu__layer");
+let menuContainerElement = document.getElementById("menu__container");
+let btnCloseMenu = document.getElementById("menu__layer-close");
+
+menuBarElement.addEventListener("click",function() {
+    menuLayerElement.classList.add("active");
+    menuContainerElement.classList.add("active-in-mobile");
+})
+
+btnCloseMenu.addEventListener("click",function() {
+    menuLayerElement.classList.remove("active");
+    menuContainerElement.classList.remove("active-in-mobile");
+})
+
+
